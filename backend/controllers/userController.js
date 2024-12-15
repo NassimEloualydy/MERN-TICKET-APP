405e3d8d36aa14ed7f35522523f36315e77cec58
nassimeloualydy <nassimaimadtahiri@gmail.com>
2024-12-15 12:26:29 +0100

    Update account user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,7 +35,28 @@ exports.logIn= async (req,res)=>{
     const token=jwt.sign({user:u[0]._id},process.env.JWT_SECRETE,{expiresIn:'30d'})
     return res.json({message:"Login With Success",token})
 }   
+
 exports.updateAccount=async (req,res)=>{
     const {first_name,last_name,email,pw,role,phone}=req.body;
-    console.log(req)
+    console.log(req.user)
+    const salt=await bcryptjs.genSalt(10)
+    const new_pw=await bcryptjs.hash(pw,salt);
+    var u=await user.find().select("-photo").and([{_id:{$ne:req.user}},{first_name},{last_name}])
+    if(u.length!=0)
+        return res.status(400).json({err:"Please the first name and the last name is laready exist !!"})
+    u=await user.find().select("-photo").and([{_id:{$ne:req.user}},{email}])
+    if(u.length!=0)
+        return res.status(400).json({err:"Please the email  is laready exist !!"})
+    u=await user.find().select("-photo").and([{_id:{$ne:req.user}},{phone}])
+    if(u.length!=0)
+        return res.status(400).json({err:"Please the phone  is laready exist !!"})
+    const User=await user.findOneAndUpdate(
+        {_id:req.user},
+        {$set:{
+            first_name,last_name,email,pw:new_pw,role,phone
+        }},{$new:true}
+    )
+    if(User)
+        return res.json({message:"User Updated with success "})
+    return res.status(400).json({err:User})
 }
